f2f7aa9461641fc4a42d527a3a14eb3ea7d3d655
jmontanholi <jonunes9326@hotmail.com.br>
2021-05-26 19:50:21 -0300

    Start rspec file

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,4 +1,11 @@
 require 'rails_helper'
 
+require 'rails_helper'
 RSpec.describe User, type: :model do 
+  it 'succeeds when you create a user with valid inputs' do
+    expect(User.create(name:"thisisvalid", 
+                email:"valid@email", 
+                password:"123456", 
+                password_confirmation:"123456")).to eq(User.find_by(name:"thisisvalid"))
+  end
 end
